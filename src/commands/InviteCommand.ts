516e3cc83fa0341f9c533fbd72d8b34d3509fc84
Raya Taufik <61215395+rayataufik@users.noreply.github.com>
2020-12-04 06:58:37 +0700

    Update InviteCommand

--- src/commands/InviteCommand.ts
+++ src/commands/InviteCommand.ts
@@ -1,17 +1,16 @@
 import { BaseCommand } from "../structures/BaseCommand";
 import { IMessage } from "../../typings";
 import { DefineCommand } from "../utils/decorators/DefineCommand";
-import { createEmbed } from "../utils/createEmbed";
 import { disableInviteCmd } from "../config";
 
 @DefineCommand({
     name: "invite",
-    description: "Get the bot's invite link",
+    description: "Get the invite link",
     usage: "{prefix}invite",
     disable: disableInviteCmd
 })
 export class InviteCommand extends BaseCommand {
     public async execute(message: IMessage): Promise<void> {
-        message.channel.send("https://discord.gg/zA3wvrv copy link ini untuk invite teman kamu ke Discord PORORO LAND");
+        message.channel.send(`${message.author.tag}, https://discord.gg/zA3wvrv copy link ini untuk invite teman kamu ke Discord PORORO LAND`);
     }
 }
